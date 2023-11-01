10dfa0ec9adac852311ac62c1a904346d3dc1293
kiddingmz <bata.isidro@gmail.com>
2023-11-01 18:07:48 +0200

    REFACTOR: 10. Depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tre
+ *
+ * @tree: pointer
+ *
+ * Return: size_t or 0
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}

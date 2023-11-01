2e08df6c5cd5e514da094a3de3c2fe721ac12846
kiddingmz <bata.isidro@gmail.com>
2023-11-01 17:29:13 +0200

    STYLE: 1. Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_insert_left - insert left child
-
- *@parent: parent
- *@value: value
+ * binary_tree_insert_left - insert left child
  *
- *Return: pointer
+ * @parent: parent
+ * @value: value
+ *
+ * Return: pointer
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)

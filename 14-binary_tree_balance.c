e1578363b0a099225a6b71771a279cc316a5b5e2
kiddingmz <bata.isidro@gmail.com>
2023-11-01 19:01:46 +0200

    CHORE: 14. Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_height - measures the height of a binary tree
- * 
+ *
  * @tree: pointer to the root node of the tree
  *
  * Return: if the tree is NULL, must return 0
@@ -25,8 +25,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  *
- * @tree:  is a pointer to the root node of the tree to measure the 
- * balance factor
+ * @tree: pointer to the root node of the tree to measure the balance factor
  *
  * Return: If tree is NULL, must return 0
  */

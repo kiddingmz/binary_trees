f37310a5b51b932665822c730be0d46d829459a4
kiddingmz <bata.isidro@gmail.com>
2023-11-01 17:33:23 +0200

    FEAT: 2. Insert right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - insert right child
+ *
+ * @parent: parent
+ * @value: value
+ *
+ * Return: pointer
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+	return (new);
+}

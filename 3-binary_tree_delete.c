4d76176db8bd033c644c67f788af296b4da8caeb
kiddingmz <bata.isidro@gmail.com>
2023-11-01 17:37:13 +0200

    FEAT: 3. Delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delete root node
+ *
+ * @tree: pointer to root noode
+ *
+ * Return: nothing
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+
+}
